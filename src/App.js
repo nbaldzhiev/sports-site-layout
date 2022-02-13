a3e78ecdf81b684dcb389fa9aa91ac08131006ea
nbaldzhiev <nenko.baldzhiev@gmail.com>
2022-02-13 16:33:55 +0200

    testing netlify deploy

--- src/App.js
+++ src/App.js
@@ -146,7 +146,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="header-title-container">
-          <Link to="/sports-site-layout/" className="header-title" onClick={() => updateTopic(defaultPhotosTopic)}>SPORT</Link>
+          <Link to="/sports-site-layout/" className="header-title" onClick={() => updateTopic(defaultPhotosTopic)}>PORT</Link>
           <div className="burger-menu-container">
             <img className="burger-menu-icon" src={burgerIcon} width="24" height="24" onClick={(event) => {
               event.preventDefault();
